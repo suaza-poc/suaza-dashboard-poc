b9e0cf3c4235d19fc2e49a0443793abbae36743b
cchavezocla <cchavezo@gmail.com>
2026-09-17 17:39:58 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://ops-dss.github.io',
-  base: process.env.BASE_PATH || '/starter-local-astro',
+  site: process.env.SITE_URL || 'https://suaza-poc.github.io',
+  base: process.env.BASE_PATH || '/suaza-dashboard-poc',
   output: 'static',
   build: {
     assets: '_astro',
